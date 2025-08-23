64d98ef065573e6d57840673c21fb143b267daf7
Christian Nikel <git@chnikel.de>
2025-08-23 15:13:44 +0200

    Service component with props

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
 <script setup lang="ts">
-import Service from './components/Service.vue';
+import Service from "./components/Service.vue";
 </script>
 
 <template>
   <div class="h-screen bg-stone-800 p-3">
-   <div class="grid grid-cols-1 md:grid-cols-2">
-    <Service />
-    <Service />
-    <Service />
-    <Service />
-   </div>
+    <div class="grid grid-cols-1 md:grid-cols-2">
+      <Service
+        title="Proxmox"
+        description="VM and container management"
+        link="http://192.168.178.23:811/login"
+        icon_url="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/nginx-proxy-manager.svg"
+      />
+      <Service
+        title="Proxmox"
+        description="VM and container management"
+        link="http://192.168.178.23:811/login"
+        icon_url="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/nginx-proxy-manager.svg"
+      />
+      <Service
+        title="Proxmox"
+        description="VM and container management"
+        link="http://192.168.178.23:811/login"
+        icon_url="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/nginx-proxy-manager.svg"
+      />
+      <Service
+        title="Proxmox"
+        description="VM and container management"
+        link="http://192.168.178.23:811/login"
+        icon_url="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/nginx-proxy-manager.svg"
+      />
+    </div>
   </div>
 </template>
--- src/components/Service.vue
+++ src/components/Service.vue
@@ -1,11 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  description: string;
+  link: string;
+  icon_url: string;
+}>();
+</script>
+
 <template>
-    <div class="p-6 flex gap-3 text-white hover:bg-stone-300/5 rounded-2xl">
-        <div class="w-16 h-16 bg-stone-700 border-stone-600 border rounded-2xl shrink-0 p-2">
-            <img class="block h-full w-full" src="https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/nginx-proxy-manager.svg">
-        </div>
-        <div>
-            <h3 class="text-lg font-semibold line-clamp-1 flex gap-2 items-center">Proxmox</h3>
-            <p class="text-sm text-fg-dimmed line-clamp-1">VM and container management</p>
-        </div>
+  <a
+    :href="link"
+    class="p-6 flex gap-3 text-white hover:bg-stone-300/5 rounded-2xl"
+  >
+    <div
+      class="w-16 h-16 bg-stone-700 border-stone-600 border rounded-2xl shrink-0 p-2"
+    >
+      <img
+        class="block h-full w-full"
+        :src="icon_url"
+      />
     </div>
+    <div>
+      <h3 class="text-lg font-semibold line-clamp-1 flex gap-2 items-center">
+        {{ title }}
+      </h3>
+      <p class="text-sm text-fg-dimmed line-clamp-1">{{ description }}</p>
+    </div>
+  </a>
 </template>

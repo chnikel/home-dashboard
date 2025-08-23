8c1254bd4f782ab50f19ea5bd7a2123102bf9142
Christian Nikel <git@chnikel.de>
2025-08-23 14:08:02 +0200

    Initial frontend

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem similique
+  aliquam nihil ipsa amet commodi ipsam iusto cumque blanditiis totam tenetur
+  corrupti incidunt nemo enim itaque maiores aut, quis laudantium.
+</template>

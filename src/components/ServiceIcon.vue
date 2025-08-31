550e62471ef0fd9338218c3e52add12cb15735e7
Christian Nikel <git@chnikel.de>
2025-08-31 23:03:30 +0200

    Service icon wrap animation

--- src/components/ServiceIcon.vue
+++ src/components/ServiceIcon.vue
@@ -7,7 +7,7 @@ defineProps<{
 
 <template>
   <div
-    class="w-16 h-16 bg-neutral-700/80 rounded-2xl shrink-0 self-center overflow-hidden"
+    class="w-16 h-16 bg-neutral-700/80 rounded-2xl shrink-0 self-center overflow-hidden transition-all"
     :class="{
       'p-2 border-neutral-600 border': !wrap,
     }"

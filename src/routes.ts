f9ee97208d562cb0e95a3e9623849640a2d88e1d
Christian Nikel <git@chnikel.de>
2025-10-21 19:19:42 +0200

    Switch to hash routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import HomeView from './pages/HomeView.vue'
 
@@ -7,6 +7,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHashHistory(),
   routes,
 })

82d96577a7a4cf99c47f6409c04a45b736dd4f6f
Christian Nikel <git@chnikel.de>
2025-10-22 03:37:59 +0200

    Add morgan for logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import morgan from "morgan";
 
 import db from "./db";
 
 const app = express();
 const port = 3000;
 
+app.use(morgan("dev"))
 app.use(express.json());
 app.use(
   cors({

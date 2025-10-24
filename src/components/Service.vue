c7a8a31e60184d501333718800ce87717938b46c
Christian Nikel <git@chnikel.de>
2025-10-24 22:49:12 +0200

    Use grid areas

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -17,7 +17,7 @@ const props = defineProps<{
 
 const sortedTags = computed(() => {
   return props.tags.sort((a, b) => {
-    return b.weight - a.weight
+    return b.weight - a.weight;
   });
 });
 </script>
@@ -26,7 +26,14 @@ const sortedTags = computed(() => {
   <a
     :href="link"
     target="_blank"
-    class="lg:p-6 flex gap-3 text-white hover:bg-neutral-300/5 rounded-2xl border"
+    class="lg:p-6 text-white hover:bg-neutral-300/5 rounded-2xl border grid gap-x-3"
+    style="
+      grid-template-columns: auto 1fr;
+      grid-template-areas:
+        'icon title'
+        'icon description'
+        'icon tags';
+    "
     :class="{
       'w-min': compact,
       'p-4': !compact,
@@ -39,20 +46,31 @@ const sortedTags = computed(() => {
       <EyeOffIcon />
     </div>
     <ServiceIcon
+      style="grid-area: icon"
       :wrap="icon_wrap"
       :url="icon_url"
     />
-    <div v-if="!compact">
-      <h3 class="text-lg font-semibold line-clamp-1 flex gap-2 items-center">
+    <template v-if="!compact">
+      <h3
+        style="grid-area: title"
+        class="text-lg font-semibold line-clamp-1 flex gap-2 items-center"
+      >
         {{ title }}
       </h3>
-      <p class="text-sm text-neutral-400 line-clamp-1">{{ description }}</p>
-      <span
-        v-for="tag in sortedTags"
-        :class="`bg-${tag.color}-100 text-${tag.color}-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-${tag.color}-900 dark:text-${tag.color}-300`"
+      <p
+        style="grid-area: description"
+        class="text-sm text-neutral-400 line-clamp-1"
       >
-        {{ tag.name }}
-      </span>
-    </div>
+        {{ description }}
+      </p>
+      <div style="grid-area: tags">
+        <span
+          v-for="tag in sortedTags"
+          :class="`bg-${tag.color}-100 text-${tag.color}-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-${tag.color}-900 dark:text-${tag.color}-300`"
+        >
+          {{ tag.name }}
+        </span>
+      </div>
+    </template>
   </a>
 </template>

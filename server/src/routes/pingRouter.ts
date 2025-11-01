3fde9ceaa8fd4b529700fc4f79977fe2db1f6d8a
Christian Nikel <git@chnikel.de>
2025-11-01 21:15:19 +0100

    Allow response codes for service

--- server/src/routes/pingRouter.ts
+++ server/src/routes/pingRouter.ts
@@ -3,32 +3,40 @@ import db from "../db";
 
 const pingRouter = express.Router();
 
+const successResponseCodes = [405, 501];
+
 async function isUrlReachable(url: string) {
   try {
-    const response = await fetch(url, { method: "GET" });
-    console.log(url, response.ok);
-    
-    return response.ok;
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+    const response = await fetch(url, { method: "HEAD" });
+    console.debug(url, response.status);
+
+    return response.ok || successResponseCodes.includes(response.status);
   } catch (err) {
     return false;
+  } finally {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "1";
   }
 }
 
 pingRouter.get("/services", async (req, res) => {
   const services = await db.allServices();
 
-  const response = await Promise.all(services
-    .filter((service) => service.enabled && service.link)
-    .map(async (service) => {
-      if (!service.link) {
-        return;
-      }
+  const response = await Promise.all(
+    services
+      .filter((service) => service.enabled && service.link)
+      .map(async (service) => {
+        if (!service.link) {
+          return;
+        }
 
-      return {
-        serviceId: service.id,
-        isReachable: await isUrlReachable(service.link)
-      };
-    }));
+        return {
+          serviceId: service.id,
+          isReachable: await isUrlReachable(service.link),
+        };
+      })
+  );
 
   res.json(response);
 });

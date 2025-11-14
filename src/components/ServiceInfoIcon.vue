8968d921c54fea976f0590651fce59a0d2d71b61
Christian Nikel <git@chnikel.de>
2025-11-14 23:55:16 +0100

    Refactor Service info icon

--- src/components/ServiceInfoIcon.vue
+++ src/components/ServiceInfoIcon.vue
@@ -1,42 +1,84 @@
 <script setup lang="ts">
+import { cn } from "@/lib/utils";
 import {
   CloudOffIcon,
   ConstructionIcon,
   HardDriveIcon,
   RadioIcon,
 } from "lucide-vue-next";
+import { computed } from "vue";
 
-defineProps<{
+type Position = "top-left" | "top-right" | "bottom-left" | "bottom-right";
+
+const props = defineProps<{
   showLiveIcon?: boolean;
   showDeprecatedIcon?: boolean;
   showDeviceIcon?: boolean;
   showDisconnectedIcon?: boolean;
+  position?: Position;
 }>();
+
+const preConfiguredIcons = {
+  disconnected: {
+    component: CloudOffIcon,
+    colorClass: "bg-red-600",
+  },
+  live: {
+    component: RadioIcon,
+    colorClass: "bg-red-800",
+  },
+  deprecated: {
+    component: ConstructionIcon,
+    colorClass: "bg-blue-800/50",
+  },
+  device: {
+    component: HardDriveIcon,
+    colorClass: "bg-neutral-800",
+  },
+};
+
+const selected = computed(() => {
+  if (props.showDisconnectedIcon) {
+    return preConfiguredIcons["disconnected"];
+  }
+  if (props.showDeviceIcon) {
+    return preConfiguredIcons["device"];
+  }
+  if (props.showDeprecatedIcon) {
+    return preConfiguredIcons["deprecated"];
+  }
+  if (props.showLiveIcon) {
+    return preConfiguredIcons["live"];
+  }
+});
+
+const position = computed(() => {
+  switch (props.position) {
+    case "top-right":
+      return "top-0 right-0 rounded-tr-xl rounded-bl-xl";
+
+    case "bottom-right":
+      return "bottom-0 right-0 rounded-tl-xl rounded-br-xl";
+
+    case "bottom-left":
+      return "bottom-0 left-0 rounded-tr-xl rounded-bl-xl";
+
+    case "top-left":
+    default:
+      return "top-0 left-0 rounded-tl-xl rounded-br-xl";
+  }
+});
 </script>
 
 <template>
   <div
-    v-if="showDisconnectedIcon"
-    class="absolute left-0 top-0 bg-red-600 rounded rounded-tl-xl rounded-br-xl p-1.5"
+    v-if="selected"
+    class="absolute rounded p-1.5"
+    :class="cn(selected.colorClass, position)"
   >
-    <CloudOffIcon :size="16" />
-  </div>
-  <div
-    v-else-if="showLiveIcon"
-    class="absolute left-0 top-0 bg-red-800 rounded rounded-tl-xl rounded-br-xl p-1.5"
-  >
-    <RadioIcon :size="16" />
-  </div>
-  <div
-    v-else-if="showDeprecatedIcon"
-    class="absolute left-0 top-0 bg-blue-800/50 rounded rounded-tl-xl rounded-br-xl p-1.5"
-  >
-    <ConstructionIcon :size="16" />
-  </div>
-  <div
-    v-else-if="showDeviceIcon"
-    class="absolute left-0 top-0 bg-neutral-800 rounded rounded-tl-xl rounded-br-xl p-1.5"
-  >
-    <HardDriveIcon :size="16" />
+    <component
+      :is="selected.component"
+      :size="16"
+    ></component>
   </div>
 </template>

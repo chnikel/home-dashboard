c7d9038005181887a1d5d7ecfdc31d15e2ed866e
Christian Nikel <git@chnikel.de>
2026-01-08 05:57:39 +0100

    update service size

--- src/components/ServiceGroup.vue
+++ src/components/ServiceGroup.vue
@@ -93,7 +93,7 @@ const data = computed<Partial<GroupDialogFormData>>(() => {
       </GroupContextMenuWrapper>
     </EditGroupWrapper>
     <hr />
-    <div class="grid p-2.5 grid-cols-[repeat(auto-fill,minmax(150px,1fr))]">
+    <div class="grid p-2.5 grid-cols-[repeat(auto-fill,minmax(130px,1fr))]">
       <slot />
     </div>
   </div>

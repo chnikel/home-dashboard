b3072e75b966fb0f3101c2508bc56b65d2b65dcc
Christian Nikel <git@chnikel.de>
2026-01-18 15:52:59 +0100

    Add route names

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,12 @@ import HomeView from "./pages/HomeView.vue";
 export const routes = [
   {
     path: "/",
+    name: "home",
     component: HomeView,
   },
   {
     path: "/settings",
+    name: "settings",
     component: () => import("./pages/SettingsView.vue"),
   },
 ];

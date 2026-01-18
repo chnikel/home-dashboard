1a1e421d7499036af3285e991cfa1ca4d1323cff
Christian Nikel <git@chnikel.de>
2026-01-18 15:48:48 +0100

    Add vue router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
+import App from "./App.vue";
+import { routes } from "./routes";
+import "./style.css";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");

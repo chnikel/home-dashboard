8bd015b311c4328728c0cf47b08aceb9c9488042
Christian Nikel <git@chnikel.de>
2026-01-18 15:48:48 +0100

    Add settings page scaffhold

---
+++ src/pages/SettingsView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio ea temporibus
+  amet non architecto magnam laborum vitae, tenetur exercitationem, assumenda,
+  ad recusandae asperiores? Ipsum libero iure cumque necessitatibus asperiores
+  nam?
+</template>
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,12 @@
 import HomeView from "./pages/HomeView.vue";
 
 export const routes = [
-  { path: '/', component: HomeView },
-]
+  {
+    path: "/",
+    component: HomeView,
+  },
+  {
+    path: "/settings",
+    component: () => import("./pages/SettingsView.vue"),
+  },
+];
